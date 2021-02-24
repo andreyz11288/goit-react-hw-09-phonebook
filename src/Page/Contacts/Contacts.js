8d3ba510490a63a502d0a40a035f2742e5b884a2
Andreyz11288 <andreyz11288@gmail.com>
2021-02-24 19:37:46 +0200

    4

--- src/Page/Contacts/Contacts.js
+++ src/Page/Contacts/Contacts.js
@@ -1,6 +1,5 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import s from './Contacts.module.css';
-// import './App2.css';
 import Contact from '../../Components/Contacts/Contact';
 import Phonebook from '../../Components/Phonebook/Phonebook';
 import Filter from '../../Components/Filter/Filter';
@@ -10,137 +9,109 @@ import { connect } from 'react-redux';
 import { addList } from '../../redux/Contacts/listOperations';
 import { getContactsItems } from '../../redux/Contacts/contacts-selectors';
 
-class Contacts extends Component {
-  state = {
-    text: '',
-    text2: '',
-    message: false,
-    message2: false,
-    alert: false,
-  };
+const Contacts = ({ contacts, onAddList }) => {
+  const [text, setText] = useState('');
+  const [text2, setText2] = useState('');
+  const [message, setMessage] = useState(false);
+  const [message2, setMessage2] = useState(false);
+  const [alert, setAlert] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    // if (this.props.upDate !== prevState.alert) {
-    //   this.setState({ alert: this.props.upDate });
-    // }
-
-    const { text, message } = this.state;
+  useEffect(() => {
     if (
       !message &&
-      this.props.contacts
-        .map(e => e.name.toLowerCase())
-        .includes(text.toLowerCase()) &&
+      contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase()) &&
       text !== ''
     ) {
-      this.setState({ alert: true });
+      setAlert(true);
     }
-    if (this.state.alert === true) {
-      this.setState({ message: true, text2: 'Такой контакт уже существует!' });
-      this.setState({ alert: false });
+    if (alert === true) {
+      setMessage(true);
+      setText2('Такой контакт уже существует!');
+      setAlert(false);
       setTimeout(() => {
-        this.setState({ message: false, text: '', text2: '' });
+        setMessage(false);
+        setText2('');
+        setText('');
       }, 3000);
-      return;
     }
-    if (this.state.alert === 'name') {
-      this.setState({
-        message: true,
-        text2: 'Контакт с таким именем уже существует!',
-      });
-      this.setState({ alert: false });
+    if (alert === 'name') {
+      setMessage(true);
+      setText2('Контакт с таким именем уже существует!');
+      setAlert(false);
       setTimeout(() => {
-        this.setState({ message: false, text: '', text2: '' });
+        setMessage(false);
+        setText2('');
+        setText('');
       }, 3000);
-      return;
     }
-    // if (this.state.alert === 'number') {
-    //   this.setState({
-    //     message: true,
-    //     text2: 'Контакт с таким номером уже существует!',
-    //   });
-    //   this.setState({ alert: false });
-    //   setTimeout(() => {
-    //     this.setState({ message: false, text: '', text2: '' });
-    //   }, 3000);
-    //   return;
-    // }
-  }
+  }, [alert, contacts, message, text]);
 
-  phonebookValue = (text, number) => {
+  const phonebookValue = (text, number) => {
     if (
       text !== '' &&
       number !== '' &&
-      this.props.contacts
-        .map(e => e.name.toLowerCase())
-        .includes(text.toLowerCase()) === false
+      contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase()) ===
+        false
     ) {
-      this.props.onAddList(text, number);
+      onAddList(text, number);
     } else {
-      this.setState({ message2: true, text2: 'Заполните все поля' });
+      setMessage2(true);
+      setText2('Заполните все поля');
       setTimeout(() => {
-        this.setState({ message2: false, text2: '' });
+        setMessage2(false);
+        setText2('');
       }, 3000);
     }
-    if (
-      this.props.contacts
-        .map(e => e.name.toLowerCase())
-        .includes(text.toLowerCase())
-    ) {
-      this.setState({ text });
-      return;
+    if (contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase())) {
+      setText(text);
     }
   };
-  alert = e => {
+  const alertFun = e => {
     if (e) {
-      console.log(e);
-      this.setState({ alert: e });
+      setAlert(e);
     }
   };
 
-  render() {
-    // console.log(this.props.upDate);
-
-    return (
-      <div className={s.App}>
-        <div className={s.notif}>
-          <CSSTransition
-            in={true}
-            appear={true}
-            classNames={s}
-            timeout={500}
-            unmountOnExit
-          >
-            <h1>Phonebook</h1>
-          </CSSTransition>
-          <div className="alert">
-            <CSSTransition
-              in={this.state.message || this.state.message2}
-              classNames="alert"
-              timeout={250}
-              unmountOnExit
-            >
-              <Alert massage={this.state.text2} />
-            </CSSTransition>
-          </div>
-        </div>
-        <Phonebook phonebookValue={this.phonebookValue} />
+  return (
+    <div className={s.App}>
+      <div className={s.notif}>
         <CSSTransition
-          in={this.props.contacts.length > 1}
-          classNames="filter"
-          timeout={250}
+          in={true}
+          appear={true}
+          classNames={s}
+          timeout={500}
           unmountOnExit
         >
-          <Filter />
+          <h1>Phonebook</h1>
         </CSSTransition>
-        <Contact alert={this.alert} />
+        <div>
+          <CSSTransition
+            in={message || message2}
+            classNames="alert"
+            timeout={250}
+            unmountOnExit
+          >
+            <Alert massage={text2} />
+          </CSSTransition>
+        </div>
       </div>
-    );
-  }
-}
+      <Phonebook phonebookValue={phonebookValue} />
+      <CSSTransition
+        in={contacts.length > 1}
+        classNames="filter"
+        timeout={250}
+        unmountOnExit
+      >
+        <Filter />
+      </CSSTransition>
+      <Contact alert={alertFun} />
+    </div>
+  );
+  // }
+};
 
 const mapStateToProps = state => ({
   contacts: getContactsItems(state),
-  // upDate: getContact(state),
 });
 
 const mapDispatchToProps = {
