651734dbaa30853a23a6393d9ac426bed706359a
andreyz11288 <andreyz11288@gmail.com>
2021-02-25 09:36:41 +0200

    5

--- src/Page/Login/Login.js
+++ src/Page/Login/Login.js
@@ -1,77 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import s from './Login.module.css';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { loginAuth } from '../../redux/Auth/authOperation';
 
-class Login extends Component {
+const Login = ({ onLogin }) => {
   // static propTypes = {
   //   phonebookValue: PropTypes.func,
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   // };
-  state = {
-    email: '',
-    password: '',
-  };
+  // state = {
+  //   email: '',
+  //   password: '',
+  // };
 
-  email = e => this.setState({ email: e.target.value });
-  password = e => this.setState({ password: e.target.value });
+  const emailFunc = e => setEmail(e.target.value);
+  const passwordFunc = e => setPassword(e.target.value);
 
-  btnClick = e => {
+  const btnClick = e => {
     e.preventDefault();
     // console.log(this.state.text, this.state.password);
-    this.props.onLogin(this.state);
+    onLogin({ email, password });
     // this.props.phonebookValue(this.state.text, this.state.number);
-    this.setState({ email: '', password: '' });
+    setEmail('');
+    setPassword('');
+    // this.setState({ email: '', password: '' });
   };
 
-  render() {
-    const { text, password } = this.state;
-    return (
-      <>
-        <h1 className={s.h1}>Aвторизация</h1>
-        <form className={s.form} onSubmit={this.btnClick}>
-          <label className={s.label}>
-            Почта
-            <input
-              className={s.input}
-              type="text"
-              value={text}
-              placeholder="Введите почту"
-              onChange={this.email}
-            />
-          </label>
+  // render() {
+  // const { text, password } = this.state;
+  return (
+    <>
+      <h1 className={s.h1}>Aвторизация</h1>
+      <form className={s.form} onSubmit={btnClick}>
+        <label className={s.label}>
+          Почта
+          <input
+            className={s.input}
+            type="text"
+            value={email}
+            placeholder="Введите почту"
+            onChange={emailFunc}
+          />
+        </label>
 
-          <label className={s.label}>
-            Пароль
-            <input
-              className={s.input}
-              type="password"
-              value={password}
-              placeholder="Введите пароль"
-              onChange={this.password}
-            />
-          </label>
-          <br />
-          <button type="submit" className={s.button}>
-            Login
-          </button>
-        </form>
-        <p className={s.p}>
-          Ещё нет учётной записи? &nbsp;
-          <NavLink
-            exact
-            to="/register"
-            className={s.navLink}
-            activeClassName={s.navLinkactive}
-          >
-            Зарегистрируйтесь
-          </NavLink>
-        </p>
-      </>
-    );
-  }
-}
+        <label className={s.label}>
+          Пароль
+          <input
+            className={s.input}
+            type="password"
+            value={password}
+            placeholder="Введите пароль"
+            onChange={passwordFunc}
+          />
+        </label>
+        <br />
+        <button type="submit" className={s.button}>
+          Login
+        </button>
+      </form>
+      <p className={s.p}>
+        Ещё нет учётной записи? &nbsp;
+        <NavLink
+          exact
+          to="/register"
+          className={s.navLink}
+          activeClassName={s.navLinkactive}
+        >
+          Зарегистрируйтесь
+        </NavLink>
+      </p>
+    </>
+  );
+  // }
+};
 
 const mapDispatchToProps = {
   onLogin: loginAuth,
