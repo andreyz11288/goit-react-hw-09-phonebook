0a904e3fae35e36ce10abb48908efb8e5b9bbaaf
Andreyz11288 <andreyz11288@gmail.com>
2021-03-01 23:55:51 +0200

    updata

--- src/Page/Contacts/Contacts.js
+++ src/Page/Contacts/Contacts.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import s from './Contacts.module.css';
 import Contact from '../../Components/Contacts/Contact';
 import Phonebook from '../../Components/Phonebook/Phonebook';
@@ -20,46 +20,32 @@ const Contacts = () => {
   const [message2, setMessage2] = useState(false);
   const [alert, setAlert] = useState(false);
 
+  const luseFanc = textAlarm => {
+    setMessage(true);
+    setText2(textAlarm);
+    setAlert(false);
+    setTimeout(() => {
+      setMessage(false);
+      setText2('');
+      setText('');
+    }, 3000);
+  };
+
+  const mapContacts = useCallback(() => {
+    contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase());
+  }, [contacts, text]);
+
   useEffect(() => {
-    if (
-      !message &&
-      contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase()) &&
-      text !== ''
-    ) {
-      setAlert(true);
+    if (!message && mapContacts && text !== '') {
+      luseFanc('Такой контакт уже существует!');
     }
-
-    if (alert === true) {
-      setMessage(true);
-      setText2('Такой контакт уже существует!');
-      setAlert(false);
-      setTimeout(() => {
-        setMessage(false);
-        setText2('');
-        setText('');
-      }, 3000);
-      return;
-    }
-
     if (alert === 'name') {
-      setMessage(true);
-      setText2('Контакт с таким именем уже существует!');
-      setAlert(false);
-      setTimeout(() => {
-        setMessage(false);
-        setText2('');
-        setText('');
-      }, 3000);
+      luseFanc('Контакт с таким именем уже существует!');
     }
-  }, [alert, text, message, contacts]);
+  }, [alert, mapContacts, message, text]);
 
   const phonebookValue = (text, number) => {
-    if (
-      text !== '' &&
-      number !== '' &&
-      contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase()) ===
-        false
-    ) {
+    if (text !== '' && number !== '' && mapContacts === false) {
       dispatch(addList(text, number));
     } else {
       setMessage2(true);
@@ -69,7 +55,7 @@ const Contacts = () => {
         setText2('');
       }, 3000);
     }
-    if (contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase())) {
+    if (mapContacts) {
       setText(text);
       return;
     }
